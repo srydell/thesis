d70ebe60fb818987708d37bd77f32a18459e3e2d
srydell <simonwrydell@gmail.com>
2018-04-11 10:45:06 +0200

    Add functionality for correlation function

--- hoshenKopelman.py
+++ hoshenKopelman.py
@@ -23,25 +23,26 @@ for i in range(N):
         # TODO: Look so these actually exists
         leftNode = (i-1, j)
         aboveNode = (i, j-1)
-        leftLabel = graph.get(leftNode).get("label")
-        aboveLabel = graph.get(aboveNode).get("label")
+        # .get() return None if not in dictionary
+        leftLabel = 0 if [i, j] in [[0, 0], [N, 0]] else graph.get(leftNode).get("label")
+        aboveLabel = 0 if [i, j] in [[0, 0], [0, M]] else graph.get(aboveNode).get("label")
 
         # Neither a label above nor to the left.
         if (leftLabel == 0) and (aboveLabel == 0)
             # Make a new, as-yet-unused cluster label.
             largest_label = largest_label + 1
-            graph[(i,j)].get("label") = largest_label;
+            graph[(i,j)]["label"] = largest_label;
 
         elif (leftLabel != 0) and (aboveLabel == 0)
             # One neighbor, to the left.
-            graph[(i,j)].get("label") = find(leftLabel);
+            graph[(i,j)]["label"] = find(leftLabel);
 
         elif (leftLabel == 0) and (aboveLabel != 0)
             # One neighbor, above.
-            graph[(i,j)].get("label") = find(aboveLabel);
+            graph[(i,j)]["label"] = find(aboveLabel);
 
         else
             # Neighbors BOTH to the left and above.
             union(leftLabel,aboveLabel)
             # Link the left and above clusters.
-            graph[(i,j)].get("label") = find(leftLabel);
+            graph[(i,j)]["label"] = find(leftLabel);
