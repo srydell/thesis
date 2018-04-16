45200100c00720abcddd32a4c518c50fb36200b7
srydell <simonwrydell@gmail.com>
2018-04-16 14:24:47 +0200

    addIfNotExists function, removeDuplicates function

--- indexClusters.py
+++ indexClusters.py
@@ -27,6 +27,18 @@ def setIndex(index, listOfSites, graph):
     for site in listOfSites:
         graph[tuple(site)]["index"] = index
 
+def addIfNotExists(key, value, dictionary):
+    """Adds the entry (key: value) to dictionary if not exists
+
+    :key: Something hashable
+    :value: Some value to be stored
+    :dictionary: dictionary
+    :returns: None
+    """
+
+    if dictionary.get(key) is None:
+        dictionary[key] = value
+
 def compareList(value, cList):
     """Checks if every entry in cList is equal to value
 
@@ -37,9 +49,46 @@ def compareList(value, cList):
 
     return all(listVal == value for listVal in cList)
 
-def indexClusters(graph):
-    """Loop through the graph and index the clusters
+def removeDuplicates(clusters, graph):
+    """Goes through each key in clusters and check if the site still has that index
 
+    :clusters: dictionary
+    :graph: dictionary
+    :returns: None
+    """
+
+    remove = []
+    for site in clusters:
+        # If the index has changed (e.g. it was overwritten for something smaller)
+        if graph[site]["index"] != clusters[site]:
+            remove.append(site)
+
+    for site in remove:
+        clusters.pop(site)
+
+def visualizeIndex(graph, N, M):
+    """Creates a matrix to visualize the indices
+
+    :graph: dictionary
+    :N: numRows
+    :M: numCols
+    :returns: NxM matrix
+    """
+
+    matrix = []
+    for i in range(N):
+        row = []
+        for j in range(M):
+            row.append(graph[(i, j)]["index"])
+        matrix.append(row)
+
+    return matrix
+
+def indexClusters(clusters, graph):
+    """Loop through the graph and index the clusters,
+    also adds the site where every new cluster is found to clusters
+
+    :clusters: dictionary
     :graph: dictionary
     :returns: None
     """
@@ -64,6 +113,7 @@ def indexClusters(graph):
                     indexHasChanged = True
                     largestIndex += 1
                     # print(f"Setting {largestIndex} on {[site, *neighbours]}")
+                    addIfNotExists(site, largestIndex, clusters)
                     setIndex(largestIndex, [site, *neighbours], graph)
                     # Go to next in the for loop through sites
                     continue
@@ -84,25 +134,6 @@ def indexClusters(graph):
                     # Go to next in the for loop through sites
                     continue
 
-def visualizeIndex(graph, N, M):
-    """Creates a matrix to visualize the indices
-
-    :graph: dictionary
-    :N: numRows
-    :M: numCols
-    :returns: NxM matrix
-    """
-
-    matrix = []
-    for i in range(N):
-        row = []
-        for j in range(M):
-            row.append(graph[(i, j)]["index"])
-        matrix.append(row)
-
-    return matrix
-
-
 if __name__ == '__main__':
     numRows = 3
     numCols = 4
@@ -118,9 +149,13 @@ if __name__ == '__main__':
     colorLinkBetween([2, 0], [2, 1], clusterGraph)
     colorLinkBetween([2, 1], [2, 2], clusterGraph)
 
+    clusters = {}
     before = copy.deepcopy(clusterGraph)
-    indexClusters(clusterGraph)
+    indexClusters(clusters, clusterGraph)
+    removeDuplicates(clusters, clusterGraph)
     after = clusterGraph
 
+    # index* is a matrix showing only the indices of each site
     indexBefore = visualizeIndex(before, numRows, numCols)
     indexAfter = visualizeIndex(after, numRows, numCols)
+
