567f4a91472559cfd4ff4bbb810aafc87a9a2de9
srydell <simonwrydell@gmail.com>
2018-04-16 13:27:27 +0200

    Finished indexClusters, it can now identify and index all clusters

--- indexClusters.py
+++ indexClusters.py
@@ -1,3 +1,32 @@
+from graphs import getLinkedNeighbours, buildGraph, colorLinkBetween
+import copy
+
+def getIndex(listOfSites, graph):
+    """Get the "index" property in graph for each site in listOfSites
+
+    :listOfSites: 1xn matrix - On the form [[i, j], [i+1, j], ...]
+    :graph: dictionary
+    :returns: 1xn matrix - Indices on the form [1, 3, ...]
+    """
+
+    indices = []
+    for site in listOfSites:
+        indices.append(graph[tuple(site)]["index"])
+
+    return indices
+
+def setIndex(index, listOfSites, graph):
+    """Get the "index" property in graph for each site in listOfSites
+
+    :index: Int - The index that will be set
+    :listOfSites: 1xn matrix - On the form [[i, j], [i+1, j], ...]
+    :graph: dictionary
+    :returns: None
+    """
+    
+    for site in listOfSites:
+        graph[tuple(site)]["index"] = index
+
 def compareList(value, cList):
     """Checks if every entry in cList is equal to value
 
@@ -6,10 +35,7 @@ def compareList(value, cList):
     :returns: Boolean
     """
 
-    for listValue in cList:
-        isAllSame = listValue == value
-
-    return isAllSame
+    return all(listVal == value for listVal in cList)
 
 def indexClusters(graph):
     """Loop through the graph and index the clusters
@@ -25,16 +51,19 @@ def indexClusters(graph):
         indexHasChanged = False
         for site in graph:
             neighbours = getLinkedNeighbours(site, graph)
+            # input(f"{site} has {neighbours} to go through.")
 
             # If there are any links
             if neighbours != []:
                 # Get a list of all the indices from the neighbours and the current site
                 clusterIndices = getIndex([site, *neighbours], graph)
+                # input(f"Got indices {clusterIndices} from sites {[site, *neighbours]}")
 
                 # If we have found a totally new cluster (all indices are 0)
                 if compareList(0, clusterIndices):
                     indexHasChanged = True
                     largestIndex += 1
+                    # print(f"Setting {largestIndex} on {[site, *neighbours]}")
                     setIndex(largestIndex, [site, *neighbours], graph)
                     # Go to next in the for loop through sites
                     continue
@@ -49,6 +78,49 @@ def indexClusters(graph):
                 # but not all neighbours have the same index, so change them 
                 if not allHaveMinIndex:
                     indexHasChanged = True
+                    # print(f"Not all clusters have the smallest index {minClusterIndex}")
+                    # print(f"Setting {minClusterIndex} on {[site, *neighbours]}")
                     setIndex(minClusterIndex, [site, *neighbours], graph)
                     # Go to next in the for loop through sites
                     continue
+
+def visualizeIndex(graph, N, M):
+    """Creates a matrix to visualize the indices
+
+    :graph: dictionary
+    :N: numRows
+    :M: numCols
+    :returns: NxM matrix
+    """
+
+    matrix = []
+    for i in range(N):
+        row = []
+        for j in range(M):
+            row.append(graph[(i, j)]["index"])
+        matrix.append(row)
+
+    return matrix
+
+
+if __name__ == '__main__':
+    numRows = 3
+    numCols = 4
+    clusterGraph = buildGraph(numRows, numCols, "dirichlet")
+    # One cluster
+    colorLinkBetween([0, 0], [1, 0], clusterGraph)
+    colorLinkBetween([1, 0], [1, 1], clusterGraph)
+    colorLinkBetween([1, 1], [1, 2], clusterGraph)
+    colorLinkBetween([1, 2], [0, 2], clusterGraph)
+    colorLinkBetween([0, 2], [0, 3], clusterGraph)
+
+    # Another cluster
+    colorLinkBetween([2, 0], [2, 1], clusterGraph)
+    colorLinkBetween([2, 1], [2, 2], clusterGraph)
+
+    before = copy.deepcopy(clusterGraph)
+    indexClusters(clusterGraph)
+    after = clusterGraph
+
+    indexBefore = visualizeIndex(before, numRows, numCols)
+    indexAfter = visualizeIndex(after, numRows, numCols)
