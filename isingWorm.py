2468d90aa4cf3fe127642d1f601232a3ea4ce08b
srydell <simonwrydell@gmail.com>
2018-04-19 15:41:14 +0200

    Only need to handle one worm at a time. This simplifies the process significantly.

--- isingWorm.py
+++ isingWorm.py
@@ -26,7 +26,7 @@ def isAccepted(K, site0, site1, gitter):
     else:
         return False
 
-def getLastTwoSites(clusterInfo, gitter):
+def getLastTwoSites(clusterInfo, gitter, index, startSites):
     """Gets the current and previous site by checking the state of the cluster ends
     If one of the ends only has one neighbour, then that neighbour is the previousSite and the end is the currentSite.
     If there are more neighbours, TODO
@@ -36,35 +36,62 @@ def getLastTwoSites(clusterInfo, gitter):
     :returns: 2 1x2 matrix - Coordinates of the current and previous site
     """
 
-    for end in clusterInfo["ends"]:
-        neighbours = getLinkedNeighbours(end, gitter)
-        if len(neighbours) == 1:
-            return end, neighbours.pop()
+    # If we got a new cluster let one of the ends be the start site
+    if startSites.get(index) is None:
+        startSites[index] = clusters["ends"][0]
 
-def simulateWormStep(K, corrFunction, currentSite, firstSite, gitter, previousSite):
+    for end in clusterInfo["ends"]:
+        if end != startSites[index]:
+            neighbours = getLinkedNeighbours(end, gitter)
+            if len(neighbours) == 1:
+                return end, neighbours.pop()
+
+def updateCorrFunc(firstSite, nextSite, corrFunction):
+    """Updates the correlation function
+
+    :corrFunction: dictionary
+    :nextSite: 1x2 matrix
+    :startSite: 1x2 matrix
+    :returns: None
+    """
+    # Update correlation function on the fly:
+    # add +1 to G(i-i0) for the open path from i0 to i
+    addIfNotExists(firstSite[0] - nextSite[0], 1, corrFunction)
+
+    # For correlation function in r
+    # addIfNotExists(sum([pow(firstSite[i] - nextSite[i], 2) for i in range(2)]), 1, Gr)
+
+def updateLoopLengths(loopLengths, index, clusters):
+    """Appends the number of sites in clusters with index to loopLengths
+
+    :loopLengths: 1xn matrix - list of loop lengths
+    :index: Int - cluster index
+    :clusters: dictionary - {index: {"ends": listOfEndSites, "sites": listOfSitesInCluster}, ...}
+    :returns: None
+    """
+    loopLengths.append(len(clusters[index]["sites"]))
+
+def simulateWormStep(K, currentSite, gitter, previousSite):
     """Moves worm head from currentSite to some new site if isAccepted.
     Chooses any neighbour from currentSite except previousSite to avoid moving 180 degrees.
     Updates the correlation function corrFunction after each step.
 
     :K: TODO
-    :corrFunction: TODO
     :currentSite: TODO
-    :firstSite: 
     :gitter: TODO
     :previousSite: TODO
     :returns: TODO
     """
 
     # Get potential next step (choose any neighbour exept previousSite)
-    newSite = getRandomNeighbour(site=currentSite, exceptSite=previousSite, graph=gitter)
+    nextSite = getRandomNeighbour(site=currentSite, exceptSite=previousSite, graph=gitter)
 
-    if isAccepted(K, currentSite, newSite, gitter):
-        colorLinkBetween(currentSite, newSite, gitter)
+    if isAccepted(K, currentSite, nextSite, gitter):
+        colorLinkBetween(currentSite, nextSite, gitter)
 
-        # Update correlation function on the fly:
-        # add +1 to G(i-i0) for the open path from i0 to i
-        # addIfNotExists(sum([pow(firstSite[i] - newSite[i], 2) for i in range(2)]), 1, Gr)
-        addIfNotExists(firstSite[0] - newSite[0], 1, corrFunction)
+        return nextSite
+
+    return currentSite
 
 def main(K, N, M, boundaryCondition):
     """Simulate ising worm algorithm
@@ -83,42 +110,55 @@ def main(K, N, M, boundaryCondition):
 
     # Initialize starting site as some random [i, j] within the gitter
     firstSite = [random.randrange(0, N), random.randrange(0, M)]
-    # Track the previous site to avoid that the current turns 180 degrees
-    previousSites = {1: firstSite}
+    print(f"First site: {firstSite}")
+
     # Get some random neighbour to form the first link
     currentSite = getRandomNeighbour(firstSite, None, gitter)
+    colorLinkBetween(currentSite, firstSite, gitter)
+
+    # Track the previous site to avoid that the current turns 180 degrees
+    previousSite = firstSite
 
     # Correlation function for r and x
     # Gr = {}
     Gx = {}
 
-    clusters = {}
-    allClustersAreLoops = False
-    colorLinkBetween(currentSite, firstSite, gitter)
+    # Loop lengths
+    loopLengths = []
 
     # Initialize clusters
+    clusters = {}
     indexClusters(clusters, gitter)
     classifyClusters(clusters, gitter)
 
+    allClustersAreLoops = False
     while not allClustersAreLoops:
-        # Check if all clusters are loops
-        if all(clusters[index]["ends"] == [] for index in clusters):
+        # Get potential next step (choose any neighbour exept previousSite)
+        nextSite = getRandomNeighbour(site=currentSite, exceptSite=previousSite, graph=gitter)
+
+        if isAccepted(K, currentSite, nextSite, gitter):
+            # Flip the weight between currentSite and nextSite
+            colorLinkBetween(currentSite, nextSite, gitter)
+
+            previousSite = currentSite
+            currentSite = nextSite
+
+        if nextSite == firstSite:
+            # Found a new loop
             allClustersAreLoops = True
+            updateLoopLengths(loopLengths, gitter[tuple(firstSite)]["index"], clusters)
 
-        else:
-            for index in clusters:
-                if clusters[index]["ends"] != []:
-                    wormHeads = clusters[index]["ends"]
-                    # currentSite = clusters[index]["ends"]
-                    # currentSite only has one neighbour (it is an ends of a cluster) NOTE: NOT TRUE CAN HAVE 3 NEIGHBOURS
-                    currentSite, previousSite = getLastTwoSites(clusters[index], gitter)
-                    print(f"Previous site: {previousSite}")
-                    input(f"Current site: {currentSite}")
-                    simulateWormStep(K, Gx, currentSite, firstSite, gitter, previousSite[0])
-            indexClusters(clusters, gitter)
-            classifyClusters(clusters, gitter)
+        # NOTE: This should always runs, even when not accepted
+        updateCorrFunc(firstSite, nextSite, Gx)
 
-    return gitter, Gx
+        # Update indexing
+        indexClusters(clusters, gitter)
+        # Update ends in clusters (classify clusters as loops and open clusters)
+        classifyClusters(clusters, gitter)
+
+    averageLoopLength = [l/len(loopLengths) for l in loopLengths]
+
+    return gitter, Gx, clusters, averageLoopLength
 
 if __name__ == '__main__':
     J = 0.5
@@ -135,7 +175,13 @@ if __name__ == '__main__':
     boundaryCondition = "dirichlet"
 
     # Run the simulation
-    gitter, Gx = main(K, N, M, boundaryCondition) 
+    gitter, Gx, clusters, averageLoopLength = main(K, N, M, boundaryCondition) 
+    print(f"Found a loop: {clusters}")
+    if len(clusters[1]["ends"]) != 0:
+        site1 = clusters[1]["ends"][0]
+        site2 = clusters[1]["ends"][1]
+        print(f"First weird site: {site1} - {gitter[site1]}")
+        print(f"Second weird site: {site2} - {gitter[site2]}")
     saveToFile = False
     if saveToFile:
         saveGraph(gitter)
