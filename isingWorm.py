669dd223b8578fad52d5a95453cced4e478530a0
srydell <simonwrydell@gmail.com>
2018-04-20 14:20:50 +0200

    Moved simulation of a worm into its own function

--- isingWorm.py
+++ isingWorm.py
@@ -52,7 +52,7 @@ def updateLoopLengths(loopLengths, index, clusters):
     """
     loopLengths.append(len(clusters[index]["sites"]))
 
-def simulateWorm(loopLengths, firstSite, K, clusters, currentSite, gitter, previousSite):
+def simulateWorm(corrFunction, loopLengths, K, clusters, gitter):
     """Starts a new worm and moves it until a loop is formed
     Chooses any neighbour from currentSite except previousSite to avoid moving 180 degrees.
     Updates the correlation function corrFunction after each step.
@@ -63,6 +63,18 @@ def simulateWorm(loopLengths, firstSite, K, clusters, currentSite, gitter, previ
     :previousSite: TODO
     :returns: TODO
     """
+    # Initialize starting site as some random [i, j] within the gitter
+    firstSite = [random.randrange(0, N), random.randrange(0, M)]
+    print(f"First site: {firstSite}")
+
+    # Get some random neighbour to form the first link
+    currentSite = getRandomNeighbour(firstSite, None, gitter)
+    colorLinkBetween(currentSite, firstSite, gitter)
+    print(f"Color link between {firstSite} and {currentSite}")
+
+    # Track the previous site to avoid that the current turns 180 degrees
+    previousSite = firstSite
+
     indexClusters(clusters, gitter)
     classifyClusters(clusters, gitter)
 
@@ -78,18 +90,20 @@ def simulateWorm(loopLengths, firstSite, K, clusters, currentSite, gitter, previ
             previousSite = currentSite
             currentSite = nextSite
 
+            print(f"Accepted site {nextSite}")
             if nextSite == firstSite:
                 # Found a new loop
                 loopFormed = True
                 updateLoopLengths(loopLengths, gitter[tuple(firstSite)]["index"], clusters)
 
         # NOTE: This should always runs, even when not accepted
-        updateCorrFunc(firstSite, nextSite, Gx)
+        updateCorrFunc(firstSite, nextSite, corrFunction)
 
         # Update indexing
         indexClusters(clusters, gitter)
         # Update ends in clusters (classify clusters as loops and open clusters)
         classifyClusters(clusters, gitter)
+        input(f"The cluster is now: {clusters}")
 
 def main(K, N, M, boundaryCondition):
     """Simulate ising worm algorithm
@@ -105,61 +119,32 @@ def main(K, N, M, boundaryCondition):
     seed = random.randrange(sys.maxsize)
     # This seed produces a self-eating loop (resulting in clusters = {})
     # seed = 5540102676881230539
+    seed = 1592744071574553462
     random.seed(seed)
 
     gitter = buildGraph(N, M, boundaryCondition)
 
-    # Initialize starting site as some random [i, j] within the gitter
-    firstSite = [random.randrange(0, N), random.randrange(0, M)]
-    print(f"First site: {firstSite}")
-
-    # Get some random neighbour to form the first link
-    currentSite = getRandomNeighbour(firstSite, None, gitter)
-    colorLinkBetween(currentSite, firstSite, gitter)
-
-    # Track the previous site to avoid that the current turns 180 degrees
-    previousSite = firstSite
-
     # Correlation function for r and x
     # Gr = {}
-    Gx = {}
+    corrFunction = {}
 
     # Loop lengths
     loopLengths = []
 
     # Initialize clusters
     clusters = {}
-    indexClusters(clusters, gitter)
-    classifyClusters(clusters, gitter)
 
-    loopFormed = False
-    while not loopFormed:
-        # Get potential next step (choose any neighbour exept previousSite)
-        nextSite = getRandomNeighbour(site=currentSite, exceptSite=previousSite, graph=gitter)
+    for _ in range(2):
+        # Move this worm until it forms a loop
+        simulateWorm(corrFunction, loopLengths, K, clusters, gitter)
 
-        if isAccepted(K, currentSite, nextSite, gitter):
-            # Flip the weight between currentSite and nextSite
-            colorLinkBetween(currentSite, nextSite, gitter)
+    # Update the average loop length
+    averageLoopLength = sum(loopLengths)/len(loopLengths)
 
-            previousSite = currentSite
-            currentSite = nextSite
+    print(f"Loop lengths: {loopLengths}")
+    print(f"Loop averages: {averageLoopLength}")
 
-            if nextSite == firstSite:
-                # Found a new loop
-                loopFormed = True
-                updateLoopLengths(loopLengths, gitter[tuple(firstSite)]["index"], clusters)
-
-        # NOTE: This should always runs, even when not accepted
-        updateCorrFunc(firstSite, nextSite, Gx)
-
-        # Update indexing
-        indexClusters(clusters, gitter)
-        # Update ends in clusters (classify clusters as loops and open clusters)
-        classifyClusters(clusters, gitter)
-
-    averageLoopLength = [l/len(loopLengths) for l in loopLengths]
-
-    return gitter, Gx, clusters, averageLoopLength, seed
+    return gitter, corrFunction, clusters, averageLoopLength, seed
 
 if __name__ == '__main__':
     J = 0.5
@@ -176,7 +161,7 @@ if __name__ == '__main__':
     boundaryCondition = "dirichlet"
 
     # Run the simulation
-    gitter, Gx, clusters, averageLoopLength, seed = main(K, N, M, boundaryCondition) 
+    gitter, corrFunction, clusters, averageLoopLength, seed = main(K, N, M, boundaryCondition) 
     print(f"Found a loop: {clusters}")
     print(f"The seed was: {seed}")
 
