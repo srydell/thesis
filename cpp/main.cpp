0c60202c32f94e2bc20212995032156debb02e22
srydell <simonwrydell@gmail.com>
2018-05-10 14:00:01 +0200

    Graph builder prototype finished. Isn't it ugly as hell?

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -45,7 +45,7 @@ int main(){
 	std::unordered_map< std::string, std::unordered_map<std::string, int> > graph;
 
 	// Weight of each site initally
-	// int startingWeight = 0;
+	int startingWeight = 0;
 
 	// Size
 	int L = 10;
@@ -76,10 +76,6 @@ int main(){
 	char delimiter = '|';
 	for (int i = 0; i < L; ++i) {
 		for (int j = 0; j < L; ++j) {
-			// Clear so it can be used in this loop
-			site.clear();
-			ss.str(std::string());
-
 			// Create "i,j"
 			ss << i << ',' << j;
 
@@ -88,32 +84,55 @@ int main(){
 
 			ss.str(std::string());
 			// Create the neighbours
-			ss << i << ',' << (j+1)%L << '|';
-			ss << i << ',' << (j-1)%L << '|';
-			ss << (i+1)%L << ',' << j << '|';
-			ss << (i-1)%L << ',' << j << '|';
+			// Check if i/j+1 is on the border
+			ss << i << ',' << (j+1)%L << delimiter;
+			ss << (i+1)%L << ',' << j << delimiter;
+			// Check if i/j-1 is on the border
+			if ((j-1) == -1) {
+				ss << i << ',' << L-1 << delimiter;
+			} else {
+				ss << i << ',' << j-1 << delimiter;
+			}
+			if ((i-1) == -1) {
+				ss << L-1 << ',' << j << delimiter;
+			} else {
+				ss << i-1 << ',' << j << delimiter;
+			}
+
+			// std::cout << "Current site: " << site << std::endl;
+			// while (std::getline(ss, neighbour, delimiter)) {
+			// 	if (neighbour.empty()) {
+			// 		ss.sync();
+			// 		ss.str(std::string());
+			// 		break;
+			// 	}
+			// 	std::cout << "This is the neighbour string: " << neighbour << std::endl;
+			// }
 
 			// Put i,j from ss into neighbours up until delimiter
 			std::getline(ss, neighbour, delimiter);
 			std::cout << "Current site: " << site << std::endl;
-			// std::cout << "This is the neighbour string: " << neighbour << std::endl;
+			std::cout << "This is the neighbour string: " << neighbour << std::endl;
+			neighbours.insert({ neighbour, startingWeight });
 			// Put i,j from ss into neighbours up until delimiter
 			std::getline(ss, neighbour, delimiter);
 			std::cout << "This is the neighbour2 string: " << neighbour << std::endl;
+			neighbours.insert({ neighbour, startingWeight });
 			// Put i,j from ss into neighbours up until delimiter
 			std::getline(ss, neighbour, delimiter);
 			std::cout << "This is the neighbour3 string: " << neighbour << std::endl;
+			neighbours.insert({ neighbour, startingWeight });
 			// Put i,j from ss into neighbours up until delimiter
 			std::getline(ss, neighbour, delimiter);
 			std::cout << "This is the neighbour4 string: " << neighbour << std::endl;
-			// right = (i, (j+1)%M)
-			// up = ((i-1)%N, j)
-			// left = (i, (j-1)%M)
-			// down = ((i+1)%N, j)
-			// neighbours = [right, up, left, down]
+			neighbours.insert({ neighbour, startingWeight });
 
-			// std::cout << site << std::endl;
-			// applyBoundaryCondition(graph, i, j, L);
+			graph.insert({ site, neighbours });
+
+			// Clear so it can be used in this loop
+			site.clear();
+			ss.str(std::string());
+			neighbours.clear();
 		}
 	}
 }
