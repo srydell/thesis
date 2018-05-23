30cec8a110fb6f1de116fa80f0ed068fdaa1639a
srydell <simonwrydell@gmail.com>
2018-05-23 14:21:31 +0200

    Fixed bug with neighbours not being updated in Site constructor.

--- cpp/source/main.cpp
+++ cpp/source/main.cpp
@@ -1,12 +1,18 @@
 #include "Graph.h"
-#include <cmath>
 #include <iostream>
-#include <vector>
+#include <string>
 
 int main(){
 	// Length of one side of the lattice
-	int length = 800;
+	int length = 4;
 	// 2D, 3D, ...
-	int dimension = 2;
+	const int dimension = 2;
 	Graph g(dimension, length);
+
+	try {
+		g.switchLinkBetween(9, 10);
+		g.switchLinkBetween(9, 10);
+	} catch(std::string error) {
+		std::cout << error << std::endl;
+	}
 }
