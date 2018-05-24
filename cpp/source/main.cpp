aa26cb6878c14b370718352ffdfe774df6e656f2
srydell <simonwrydell@gmail.com>
2018-05-24 13:09:20 +0200

    Expanding main

--- cpp/source/main.cpp
+++ cpp/source/main.cpp
@@ -4,12 +4,6 @@
 #include <string>
 
 int main(){
-	// Length of one side of the lattice
-	int length = 4;
-	// 2D, 3D, ...
-	const int dimension = 2;
-	Graph g(dimension, length);
-
 	// Type of random number distribution
 	std::uniform_real_distribution<double> dist(0.0, 1.0);  //(min, max)
 
@@ -20,14 +14,23 @@ int main(){
 
     // std::cout << dist(rng) << std::endl;
 
+	// Length of one side of the lattice
+	int length = 4;
+	// 2D, 3D, ...
+	const int dimension = 2;
+	Graph g(dimension, length);
+
 	// Simulation
 	try {
 		std::vector<unsigned> neighbours;
 
+		std::cout << "Switch 9 and 10" << "\n";
 		g.SwitchLinkBetween(9, 10);
+		std::cout << "Switch 9 and 13" << "\n";
 		g.SwitchLinkBetween(9, 13);
 		neighbours = g.GetLinkedNeighbours(9);
 
+		std::cout << "Switch 9 and 13" << "\n";
 		g.SwitchLinkBetween(9, 13);
 		neighbours = g.GetLinkedNeighbours(9);
 
@@ -37,9 +40,10 @@ int main(){
 
 		unsigned rNeighbour = g.GetRandomNeighbour(9, 10, dist(rng));
 		std::cout << rNeighbour << "\n";
+		unsigned notNeighbour = g.GetRandomNeighbour(9, 11, dist(rng));
+		std::cout << notNeighbour << "\n";
 
 	} catch(std::string error) {
 		std::cout << error << std::endl;
 	}
-
 }
