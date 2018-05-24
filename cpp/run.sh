c30001634145a24d56eb819aad98afe0548b176d
srydell <simonwrydell@gmail.com>
2018-05-24 11:45:17 +0200

    Pipe uninteresting output to /dev/null

--- cpp/run.sh
+++ cpp/run.sh
@@ -4,16 +4,16 @@ cd ./build || exit
 
 if [[ "$1" == build ]]; then
 	# Build the makefiles
-	cmake ../
+	cmake ../ > /dev/null 2>&1
 	
 	# Run if build succeeds
-	if cmake --build .; then
+	if cmake --build . > /dev/null 2>&1; then
 		time ./IsingWorm ;
 	fi
 	exit
 fi
 
 # Run if build succeeds
-if cmake --build .; then
+if cmake --build . > /dev/null 2>&1; then
 	time ./IsingWorm;
 fi
