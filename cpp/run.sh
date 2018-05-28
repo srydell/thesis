dfbdf7eb2b75d24c0d1e1d3a76310ddd56b4b85f
srydell <simonwrydell@gmail.com>
2018-05-28 11:57:10 +0200

    run.sh build now regenerates doxygen config aswell

--- cpp/run.sh
+++ cpp/run.sh
@@ -2,18 +2,29 @@
 
 cd ./build || exit
 
-if [[ "$1" == build ]]; then
-	# Build the makefiles
-	cmake ../
-	
+function cmakeRunProject {
 	# Run if build succeeds
 	if cmake --build .; then
 		time ./IsingWorm ;
 	fi
+}
+
+if [[ "$1" == build ]]; then
+	# Build the makefiles
+	cmake ../
+
+	# Regenerate the doxygen documentation
+	# In build/doxygen
+	cd doxygen || exit
+	doxygen ../doxygen.conf &> /dev/null
+
+	# Go back to build
+	cd ../ || exit
+	
+	# Run the code
+	cmakeRunProject
 	exit
 fi
 
-# Run if build succeeds
-if cmake --build .; then
-	time ./IsingWorm;
-fi
+# If not build, then run it via cmake
+cmakeRunProject
