0b8b8fe52a1b414ec6828b963304ce3c6ca63001
srydell <simonwrydell@gmail.com>
2018-06-05 16:37:36 +0200

    Tests now see the normal header files

--- cpp/test/my_test.cpp
+++ cpp/test/my_test.cpp
@@ -1,16 +1,14 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
+#include "Graph.h"
 
 TEST_CASE("A test case", "[testing]") {
-	REQUIRE( 1 == 1 );
+	Graph lattice = Graph(4, 4);
+	REQUIRE( 2 == 2 );
 }
 
 TEST_CASE("simple test case", "[testing]") {
 	REQUIRE( 2 == 2 );
-}
-
-TEST_CASE("teh simplest test case", "[testing]") {
-	REQUIRE( 4 == 3 );
 }
 
 TEST_CASE("Ajkldsf test case", "[testing]") {
