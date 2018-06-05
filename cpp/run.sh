2fd44b76696d187f0cffb7a68b01cbbab7810a31
srydell <simonwrydell@gmail.com>
2018-06-05 11:51:13 +0200

    Confusion over test and tests over?

--- cpp/run.sh
+++ cpp/run.sh
@@ -4,7 +4,6 @@ cd ./build || exit
 
 function run_project {
 	# Run binary build succeeds
-	# if cmake --build .; then
 	if make --no-print-directory "$1"; then
 		time ./IsingWorm ;
 	fi
@@ -26,10 +25,12 @@ if [[ "$1" == build ]]; then
 elif [[ "$1" == test ]]; then
 	# run_project test
 
+	# What it is called in the CMakeLists.txt
+	custom_test_name=tests
 	# Recompile everything if necessary
-	make tests &> /dev/null
+	make $custom_test_name &> /dev/null
 	# Run the tests
-	ctest --output-on-failure
+	ctest --output-on-failure $custom_test_name
 else
 	# On clean make, just run the project
 	run_project IsingWorm
