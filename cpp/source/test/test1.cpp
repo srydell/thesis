c59362ac82ec13f007d52a10b15c6089ed0534d4
srydell <simonwrydell@gmail.com>
2018-06-18 13:10:16 +0200

    Started fixing testing

--- cpp/source/test/test1.cpp
+++ cpp/source/test/test1.cpp
@@ -1,15 +1,35 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "cmath"
 #include "Graph.h"
 
 unsigned int Factorial( unsigned int number ) {
     return number <= 1 ? number : Factorial(number-1)*number;
 }
 
-TEST_CASE( "Factorials are computed", "[factorial]" ) {
-    REQUIRE( Factorial(1) == 1 );
-    REQUIRE( Factorial(2) == 2 );
-    REQUIRE( Factorial(3) == 6 );
-    REQUIRE( Factorial(10) == 3628800 );
-    REQUIRE( 10 == 3628800 );
+TEST_CASE( "All neighbours are neighbours to each other", "[Graph]" ) {
+	unsigned dimension = 2;
+	unsigned length = 4;
+	int nulltime = time(nullptr);
+	srand((unsigned)nulltime);
+
+	unsigned long mSeed = rand();
+	Graph lattice = Graph(dimension, length, mSeed);
+
+	lattice.SwitchLinkBetween(0, 1);
+	lattice.SwitchLinkBetween(13, 1);
+	lattice.SwitchLinkBetween(13, 14);
+	lattice.SwitchLinkBetween(14, 10);
+	lattice.SwitchLinkBetween(6, 10);
+	lattice.SwitchLinkBetween(7, 6);
+	lattice.SwitchLinkBetween(7, 4);
+	lattice.SwitchLinkBetween(0, 4);
+	for (unsigned i = 0; i < std::pow(length, dimension)-1; ++i) {
+		std::vector<unsigned> neighbours;
+		lattice.GetLinkedNeighbours(i, neighbours);
+		for (auto n : neighbours) {
+			std::cout << n << "\n";
+		}
+	}
+	// REQUIRE( 4 == 5 );
 }
