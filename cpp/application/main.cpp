75854fb565b7210164329cb2f579760dc51bc97d
srydell <simonwrydell@gmail.com>
2018-06-29 13:57:26 +0200

    utils container to ostream template
    pyplotting a textfile
    
    moved simulations to IsingSimulation

--- cpp/application/main.cpp
+++ cpp/application/main.cpp
@@ -2,6 +2,7 @@
 #include "IsingWorm.h"
 #include "utils.h"
 #include <cmath>
+#include <fstream>
 #include <iostream>
 #include <string>
 #include <unordered_map>
@@ -10,7 +11,9 @@ int main(){
 	// Bond strength
 	double J = 0.5;
 	// Temperature
-	double T = 1;
+	// Critical temperature for Ising
+	// 2 / (ln(1 + sqrt(2)))
+	double T = 2.269185314213;
 	double K = J/T;
 	// Length of one side of the lattice
 	unsigned length = std::pow(2, 4);
@@ -29,77 +32,72 @@ int main(){
 	// unsigned long seed = 1692706254;
 
 	std::cout << "Seed is: " << seed << "\n";
+	std::cout << "Length is: " << length << "\n";
+
+	// Initialize the correlation function
+	std::unordered_map<unsigned, unsigned> correlation_func;
 
 	Graph lattice(dimension, length, seed);
 
-	// Simulation
+	// This will store all of the loop lengths
+	std::vector<unsigned> loop_lengths;
+
+	// Initialize the clusters map
+	std::unordered_map<unsigned, std::vector<unsigned>> clusters;
+	lattice.IndexClusters(clusters);
+
 	try {
 
-		// Get the first site for this simulation
-		unsigned first_site = lattice.GetRandomSite();
-		unsigned *null = nullptr;
-		// Get some random neighbour to form the first link
-		unsigned current_site = lattice.GetRandomNeighbour(first_site, null);
-		// Track the previous site to avoid that the current turns 180 degrees
-		unsigned previous_site = first_site;
-		// Form the first link
-		lattice.SwitchLinkBetween(first_site, current_site);
+		std::ofstream outfile;
 
-		// Initialize the correlation function
-		std::unordered_map<unsigned, unsigned> correlation_func;
-		UpdateCorrelationFunction(current_site, first_site, length, correlation_func);
+		outfile.open("test.txt", std::ios_base::app);
+		for (unsigned i = 0; i < 10; ++i) {
+			// Create a new graph
+			unsigned long seed = rand();
+			Graph lattice(dimension, length, seed);
 
-		// Initialize the clusters map
-		std::unordered_map<unsigned, std::vector<unsigned>> clusters;
+			// This will store all of the loop lengths
+			std::vector<unsigned> loop_lengths;
+
+			unsigned num_simulations = 10000;
+			// unsigned num_simulations = 10;
+			std::vector<unsigned> max_loop_lengths;
+			max_loop_lengths.reserve(num_simulations);
+
+			for (unsigned i = 0; i < num_simulations; ++i) {
+				// Simulation
+				IsingSimulation(lattice, correlation_func, length, K);
+
+				// Update indexing
+				lattice.IndexClusters(clusters);
+
+				UpdateLoopLengths(loop_lengths, clusters, lattice);
+				// std::cout << *std::max_element(loop_lengths.begin(), loop_lengths.end()) << "\n";
+				max_loop_lengths.push_back(*std::max_element(loop_lengths.begin(), loop_lengths.end()));
+
+			}
+			std::cout << "Finished with graph number: " << i << "\n";
+			write_container(max_loop_lengths, outfile, ' ');
+			outfile << '\n';
+		}
+
+		// Update indexing
 		lattice.IndexClusters(clusters);
 
-		// This will store all of the loop lengths
-		std::vector<unsigned> loop_lengths;
+		UpdateLoopLengths(loop_lengths, clusters, lattice);
+
 		// This will store the average loop length according to Mats' notes
-		double average_loop_length = 0;
+		double average_loop_length = GetAverageLoopLength(loop_lengths, K);
 
-		bool loop_formed = 0;
-		while (!loop_formed) {
-			// previous_site is passed as a pointer since when null, no site is excepted from being chosen
-			unsigned next_site = lattice.GetRandomNeighbour(current_site, &previous_site);
-
-			// std::cout << "Next site is: " << next_site << "\n";
-
-			auto rand_num = lattice.GetRandomNum();
-			if (IsAccepted(K, lattice.GetLink(current_site, next_site), rand_num)) {
-
-				// std::cout << "Got accepted!" << "\n";
-				// std::cout << "Switch link between sites: " << current_site << " and " << next_site << "\n";
-
-				// Flip the weight between currentSite and nextSite
-				lattice.SwitchLinkBetween(current_site, next_site);
-				previous_site = current_site;
-				current_site = next_site;
-				// If we have found a loop
-				if (next_site == first_site) {
-					std::cout << "Found a loop!" << "\n";
-					loop_formed = 1;
-					// Update indexing
-					lattice.IndexClusters(clusters);
-					// Update loop lengths
-					UpdateLoopLengths(loop_lengths, clusters, lattice);
-					average_loop_length = GetAverageLoopLength(loop_lengths, K);
-					std::cout << "Average loop length is: " << average_loop_length << "\n";
-
-					std::cout << "Cluster is:" << "\n";
-					lattice.PrintClusters(clusters);
-					
-					std::cout << "Loop lengths are:" << "\n";
-					std::cout << "(";
-					for (auto l : loop_lengths) {
-						std::cout << l << ", ";
-					}
-					std::cout << ")" << "\n";
-
-				}
-				UpdateCorrelationFunction(first_site, next_site, length, correlation_func);
-			}
+		std::cout << "Cluster is:" << "\n";
+		lattice.PrintClusters(clusters);
+		std::cout << "Average loop length is: " << average_loop_length << "\n";
+		std::cout << "Loop lengths are:" << "\n";
+		std::cout << "(";
+		for (auto l : loop_lengths) {
+			std::cout << l << ", ";
 		}
+		std::cout << ")" << "\n";
 
 	} catch(std::string& error) {
 		std::cout << error << "\n";
--- cpp/source/IsingWorm.cpp
+++ cpp/source/IsingWorm.cpp
@@ -159,76 +159,43 @@ void UpdateLoopLengths(std::vector<unsigned> &loop_lengths, std::unordered_map<u
 * @return: void
 */
 void IsingSimulation(Graph & lattice, std::unordered_map<unsigned, unsigned> &correlation_func, unsigned length, double K) {
+	// Get the first site for this simulation
+	unsigned first_site = lattice.GetRandomSite();
+	unsigned *null = nullptr;
+	// Get some random neighbour to form the first link
+	unsigned current_site = lattice.GetRandomNeighbour(first_site, null);
+	// Track the previous site to avoid that the current turns 180 degrees
+	unsigned previous_site = first_site;
+	// Form the first link
+	lattice.SwitchLinkBetween(first_site, current_site);
 
-	// Simulation
-	try {
+	// Initialize the correlation function
+	UpdateCorrelationFunction(current_site, first_site, length, correlation_func);
 
-		// Get the first site for this simulation
-		unsigned first_site = lattice.GetRandomSite();
-		unsigned *null = nullptr;
-		// Get some random neighbour to form the first link
-		unsigned current_site = lattice.GetRandomNeighbour(first_site, null);
-		// Track the previous site to avoid that the current turns 180 degrees
-		unsigned previous_site = first_site;
-		// Form the first link
-		lattice.SwitchLinkBetween(first_site, current_site);
+	bool loop_formed = 0;
+	while (!loop_formed) {
+		// previous_site is passed as a pointer since when null, no site is excepted from being chosen
+		unsigned next_site = lattice.GetRandomNeighbour(current_site, &previous_site);
 
-		// Initialize the correlation function
-		UpdateCorrelationFunction(current_site, first_site, length, correlation_func);
+		// std::cout << "Next site is: " << next_site << "\n";
 
-		// Initialize the clusters map
-		std::unordered_map<unsigned, std::vector<unsigned>> clusters;
-		lattice.IndexClusters(clusters);
+		auto rand_num = lattice.GetRandomNum();
+		if (IsAccepted(K, lattice.GetLink(current_site, next_site), rand_num)) {
 
-		// This will store all of the loop lengths
-		std::vector<unsigned> loop_lengths;
-		// This will store the average loop length according to Mats' notes
-		double average_loop_length = 0;
+			// std::cout << "Got accepted!" << "\n";
+			// std::cout << "Switch link between sites: " << current_site << " and " << next_site << "\n";
 
-		bool loop_formed = 0;
-		while (!loop_formed) {
-			// previous_site is passed as a pointer since when null, no site is excepted from being chosen
-			unsigned next_site = lattice.GetRandomNeighbour(current_site, &previous_site);
+			// Flip the weight between currentSite and nextSite
+			lattice.SwitchLinkBetween(current_site, next_site);
+			previous_site = current_site;
+			current_site = next_site;
+			// If we have found a loop
+			if (next_site == first_site) {
+				// std::cout << "Found a loop!" << "\n";
+				loop_formed = 1;
 
-			// std::cout << "Next site is: " << next_site << "\n";
-
-			auto rand_num = lattice.GetRandomNum();
-			if (IsAccepted(K, lattice.GetLink(current_site, next_site), rand_num)) {
-
-				// std::cout << "Got accepted!" << "\n";
-				// std::cout << "Switch link between sites: " << current_site << " and " << next_site << "\n";
-
-				// Flip the weight between currentSite and nextSite
-				lattice.SwitchLinkBetween(current_site, next_site);
-				previous_site = current_site;
-				current_site = next_site;
-				// If we have found a loop
-				if (next_site == first_site) {
-					std::cout << "Found a loop!" << "\n";
-					loop_formed = 1;
-					// Update indexing
-					lattice.IndexClusters(clusters);
-					// Update loop lengths
-					UpdateLoopLengths(loop_lengths, clusters, lattice);
-					average_loop_length = GetAverageLoopLength(loop_lengths, K);
-					std::cout << "Average loop length is: " << average_loop_length << "\n";
-
-					std::cout << "Cluster is:" << "\n";
-					lattice.PrintClusters(clusters);
-					
-					std::cout << "Loop lengths are:" << "\n";
-					std::cout << "(";
-					for (auto l : loop_lengths) {
-						std::cout << l << ", ";
-					}
-					std::cout << ")" << "\n";
-
-				}
-				UpdateCorrelationFunction(first_site, next_site, length, correlation_func);
 			}
+			UpdateCorrelationFunction(first_site, next_site, length, correlation_func);
 		}
-
-	} catch(std::string& error) {
-		std::cout << error << "\n";
 	}
 }
