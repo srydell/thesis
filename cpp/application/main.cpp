1a1aeef0d9f9cfd03e146440ba757599cadfbb67
srydell <simonwrydell@gmail.com>
2018-07-12 14:07:26 +0200

    Only log the largest worm

--- cpp/application/main.cpp
+++ cpp/application/main.cpp
@@ -38,30 +38,20 @@ int main(/*int argc, char** argv*/) {
 
 		// std::string final_sizes_filename = argv[0];
 		std::string final_sizes_filename = "final_sizes.txt";
-		std::string avg_sizes_filename = "avg_sizes.txt";
-		std::string avg_tanh_sizes_filename = "avg_tanh_sizes.txt";
 
 		// To store results
 		std::ofstream final_sizes_file;
-		std::ofstream avg_sizes_file;
-		std::ofstream avg_tanh_sizes_file;
 
 		// Open all the datafiles
-		avg_sizes_file.open(avg_sizes_filename, std::ios_base::app);
 		final_sizes_file.open(final_sizes_filename, std::ios_base::app);
-		avg_tanh_sizes_file.open(avg_tanh_sizes_filename, std::ios_base::app);
 
 		unsigned max_length_exponent = 7;
 
 		std::vector<unsigned> max_loop_lengths;
 		max_loop_lengths.reserve(max_length_exponent);
-		std::vector<unsigned> avg_loop_lengths;
-		avg_loop_lengths.reserve(max_length_exponent);
-		std::vector<unsigned> avg_tanh_loop_lengths;
-		avg_tanh_loop_lengths.reserve(max_length_exponent);
 
 		// How many different sizes of the simulation should run (L = 2^i)
-		for (unsigned i = 2; i < max_length_exponent; ++i) {
+		for (unsigned i = max_length_exponent-1; i < max_length_exponent; ++i) {
 			// Create a new graph
 			unsigned length = std::pow(2, i);
 			unsigned long seed = rand();
@@ -69,8 +59,8 @@ int main(/*int argc, char** argv*/) {
 		
 			std::cout << "Seed is: " << seed << "\n";
 
-			// This will store all of the loop lengths
-			std::vector<unsigned> loop_lengths;
+			// This will store (Cluster index: Loop lengths)
+			std::unordered_map<unsigned, unsigned> loop_lengths;
 		
 			// Initialize the correlation function
 			std::unordered_map<unsigned, unsigned> correlation_func;
@@ -80,7 +70,8 @@ int main(/*int argc, char** argv*/) {
 		
 			unsigned num_worms_started = 10000;
 			// Run the simulation until it hopefully goes to equilibrium
-			for (unsigned i = 0; i < num_worms_started; ++i) {
+			for (unsigned j = 0; j < num_worms_started; ++j) {
+				std::cout << "Worm number:" << j << "\n";
 				IsingSimulation(lattice, correlation_func, length, K);
 			}
 		
@@ -89,52 +80,41 @@ int main(/*int argc, char** argv*/) {
 		
 			UpdateLoopLengths(loop_lengths, clusters, lattice);
 		
+			// Find the sites corresponding to the largest worm
+			std::vector<unsigned> largest_worm = clusters[GetMaximumMapIndex(loop_lengths)];
+
+			// Split up the graph in boxes of decreasing sizes
+			std::unordered_map<unsigned, std::vector<unsigned>> blocks;
+			lattice.DivideGraph(blocks);
+
+			std::unordered_map<unsigned, unsigned> side_length_and_num_occupied;
+			lattice.GetBoxDimension(blocks, side_length_and_num_occupied, largest_worm);
+
+			std::cout << "Box dimensions:" << "\n";
+			for (auto& sl_and_no : side_length_and_num_occupied) {
+				std::cout << sl_and_no.first << " : ";
+				std::cout << sl_and_no.second << "\n";
+			}
+		
 			// std::cout << *std::max_element(loop_lengths.begin(), loop_lengths.end()) << "\n";
 			// After num_simulations the max length has converged and we can append it
-			max_loop_lengths.push_back(*std::max_element(loop_lengths.begin(), loop_lengths.end()));
-			avg_loop_lengths.push_back(
-					std::accumulate(loop_lengths.begin(), loop_lengths.end(), 0.0)/loop_lengths.size()
-					);
-			avg_tanh_loop_lengths.push_back(GetAverageLoopLength(loop_lengths, K));
+			// max_loop_lengths.push_back(*std::max_element(loop_lengths.begin(), loop_lengths.end()));
 		
-			std::cout << "Loop lengths:" << "\n";
-			for (auto& ll : loop_lengths) {
-				std::cout << ll << ", ";
-			}
-			std::cout << "\nAverage loop lengths:" << "\n";
-			for (auto& ll : avg_loop_lengths) {
-				std::cout << ll << ", ";
-			}
-			std::cout << "\n";
+			// std::cout << "Loop lengths:" << "\n";
+			// for (auto& ll : loop_lengths) {
+			// 	std::cout << ll.second << ", ";
+			// }
+			// std::cout << "\n";
 		
 			std::cout << "Finished with graph of size: " << std::pow(2, i) << "\n";
 		
 		}
 		write_container(max_loop_lengths, final_sizes_file, ' ');
-		write_container(avg_loop_lengths, avg_sizes_file, ' ');
-		write_container(avg_tanh_loop_lengths, avg_tanh_sizes_file, ' ');
 		// Make a new row for the next run
 		final_sizes_file << '\n';
-		avg_sizes_file << '\n';
-		avg_tanh_sizes_file << '\n';
-
-		// Update indexing
-		// lattice.IndexClusters(clusters);
-
-		// UpdateLoopLengths(loop_lengths, clusters, lattice);
 
 		// This will store the average loop length according to Mats' notes
 		// double average_loop_length = GetAverageLoopLength(loop_lengths, K);
-
-		// std::cout << "Cluster is:" << "\n";
-		// lattice.PrintClusters(clusters);
-		// std::cout << "Average loop length is: " << average_loop_length << "\n";
-		// std::cout << "Loop lengths are:" << "\n";
-		// std::cout << "(";
-		// for (auto l : loop_lengths) {
-		// 	std::cout << l << ", ";
-		// }
-		// std::cout << ")" << "\n";
 
 	} catch(std::string& error) {
 		std::cout << error << "\n";
