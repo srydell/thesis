2270d2950297a32b240f734fd43b7c02e7b61b01
srydell <simonwrydell@gmail.com>
2018-08-28 11:53:12 +0200

    Added helper function GetUniqueFile and used it in main.
    Also added a file output to total_worm_length

--- cpp/application/main.cpp
+++ cpp/application/main.cpp
@@ -12,14 +12,6 @@
 
 int main(/*int argc, char** argv*/) {
 	try {
-
-		// If no input
-		// if (argc == 1) {
-		// 	std::string error_no_data_file_name = "Please enter a name for a file to save data to";
-		// 	throw error_no_data_file_name;
-		// }
-		// std::cout << "Got pass the argc" << "\n";
-
 		// Bond strength
 		// Temperature
 		// Critical temperature for Ising
@@ -36,22 +28,18 @@ int main(/*int argc, char** argv*/) {
 		double K = J/T;
 
 		int nulltime = time(nullptr);
-		srand((unsigned)nulltime);
+		srand((unsigned)nulltime + getpid());
 
 		// Create data files
-		
-		std::ofstream susceptibility_file;
-		std::stringstream ss;
-		ss <<  "susceptibility" << getpid() << ".txt";
-		std::string susceptibility_filename = ss.str();
-		susceptibility_file.open(susceptibility_filename, std::ios_base::app);
+		auto susceptibility_file = GetUniqueFile("susceptibility");
+		auto total_worm_length_file = GetUniqueFile("total_worm_length");
 
-		unsigned max_length_exponent = 8;
 		// unsigned max_length_exponent = 3;
+		unsigned max_length_exponent = 8;
 
 		// Run the simulation for each size 
 		// 1 run takes about 22 seconds
-		int num_sim = 3436;
+		int num_sim = 1;
 		for (int multiplier = 0; multiplier < num_sim; ++multiplier) {
 			std::vector<unsigned> max_loop_lengths;
 			max_loop_lengths.reserve(max_length_exponent);
@@ -59,76 +47,81 @@ int main(/*int argc, char** argv*/) {
 			std::vector<long double> susceptibility;
 			susceptibility.reserve(max_length_exponent);
 
+			std::vector<long int> total_worm_length;
+			total_worm_length.reserve(max_length_exponent);
+
 			// How many different sizes of the simulation should run (L = 2^i)
 			for (unsigned i = 2; i < max_length_exponent; ++i) {
 				// Create a new graph
 				unsigned length = std::pow(2, i);
 				unsigned long seed = rand();
 				Graph lattice(dimension, length, seed + getpid());
-			
+
 				// std::cout << "Seed is: " << seed << "\n";
 			
 				// This will store {Cluster index: Loop lengths}
 				std::unordered_map<unsigned, unsigned> loop_lengths;
-			
+
 				// Initialize the clusters map
 				std::unordered_map<unsigned, std::vector<unsigned>> clusters;
-			
-				unsigned num_worms_started = 10000;
+
+				int num_worms_started = 10000;
+
+				// Run the updates and throw away the results
+				WarmUp(num_worms_started * i, lattice, K);
+
 				// Store how many total steps are accepted
 				long double num_steps = 0.0;
+
 				// Run the simulation until it hopefully goes to equilibrium
-				for (unsigned j = 0; j < num_worms_started; ++j) {
-			
-					// std::cout << "Worm number:" << j << "\n";
-			
+				for (int j = 0; j < num_worms_started; ++j) {
 					num_steps += IsingSimulation(lattice, K);
-					// std::cout << "Required number of steps: " << num_steps << "\n";
-					// std::string s;
-					// std::cin >> s;
 				}
-			
+
 				// Update indexing
 				lattice.HKIndex(clusters);
-			
+
 				UpdateLoopLengths(loop_lengths, clusters, lattice);
-			
+
 				// Find the sites corresponding to the largest worm
 				std::vector<unsigned> largest_worm = clusters[GetMaximumMapIndex(loop_lengths)];
-			
+
 				// Split up the graph in boxes of decreasing sizes
 				std::unordered_map<unsigned, std::vector<unsigned>> blocks;
 				lattice.DivideGraph(blocks);
-			
+
 				std::unordered_map<unsigned, unsigned> side_length_and_num_occupied;
 				lattice.GetBoxDimension(blocks, side_length_and_num_occupied, largest_worm);
-			
+
 				std::cerr << "L=" << length << ":\n";
 				for (auto& sl_and_no : side_length_and_num_occupied) {
 					std::cerr << sl_and_no.first << " ";
 					std::cerr << sl_and_no.second << "\n";
 				}
-			
-				// std::cout << *std::max_element(loop_lengths.begin(), loop_lengths.end()) << "\n";
+
 				// After num_simulations the max length has converged and we can append it
 				max_loop_lengths.push_back(loop_lengths[GetMaximumMapIndex(loop_lengths)]);
-			
+
 				// susc = 1 / T * sum_i(g(i)) = 1 / T * ((step taken)/(worms started))
 				susceptibility.push_back(num_steps / (num_worms_started * T));
-			
-				// std::cout << "Finished with graph of size: " << std::pow(2, i) << "\n";
-			
+				
+				unsigned total_length = 0;
+				for (auto& index_and_llength : loop_lengths) {
+					total_length += index_and_llength.second;
+				}
+				total_worm_length.push_back(total_length);
 			}
 			write_container(max_loop_lengths, std::cout, ' ');
 			write_container(susceptibility, susceptibility_file, ' ');
+			write_container(total_worm_length, total_worm_length_file, ' ');
 			// Make a new row for the next run
 			std::cout << '\n';
 			susceptibility_file << '\n';
-			
+			total_worm_length_file << '\n';
+
 			// This will store the average loop length according to Mats' notes
 			// double average_loop_length = GetAverageLoopLength(loop_lengths, K);
 		}
-
 	} catch(std::string& error) {
 		std::cout << error << "\n";
 	}
