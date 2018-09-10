65fb9232e487bce58cfb055663513403c8c95f89
srydell <simonwrydell@gmail.com>
2018-09-10 10:07:02 +0200

    Reorganized cpp model for xy

---
+++ cpp/source/xy/IsingGraphUtils.cpp
@@ -0,0 +1,83 @@
+#include "Graph.h"
+#include "utils.h"
+#include <cmath>
+#include <iostream>
+#include <random>
+#include <sstream>
+#include <vector>
+
+/**
+* @brief: Change value between site0 and site1 in mGraph so that 0 <-> 1
+*
+* @param: int site0
+*       : int site1
+*
+* @return: void
+*/
+void Graph::SwitchLinkBetween(int site0, int site1) {
+	// Check if sites are in mGraph
+	if (IsInGraph(site0) && IsInGraph(site1)) {
+
+		// Check if siteX is a neighbour to siteY
+		// Will be:
+		//     pair<int, bool> - If found
+		//     neighbours.end()     - If not found
+
+		// Get the ends of the list to check links against
+		// auto end0 = mGraph[site0].neighbours.end();
+		// auto end1 = mGraph[site1].neighbours.end();
+
+		// auto link1 = mGraph[site1].neighbours.find(site0);
+		if (AreNeighbours(site0, site1)) {
+			// Get the link weights
+			auto link0 = mGraph[site0].neighbours[site1];
+			auto link1 = mGraph[site1].neighbours[site0];
+
+			// Switch the links
+			mGraph[site0].neighbours[site1] = link0 ? 0 : 1;
+			mGraph[site1].neighbours[site0] = link1 ? 0 : 1;
+
+		} else {
+			// Error handling for siteX and siteY not neighbours
+			std::stringstream ss;
+			ss << "Call to Graph::SwitchLinkBetween failed since "
+				<< site0 << " and " << site1 << " are not in neighbours." << "\n";
+			throw ss.str();
+		}
+
+	} else {
+		// Error handling for site not in mGraph
+		std::stringstream ss;
+		ss << "Call to Graph::SwitchLinkBetween failed since "
+			<< site0 << " and/or " << site1 << " are not in mGraph." << "\n";
+		throw ss.str();
+	}
+}
+
+/**
+* @brief: Return the link between site0 and site1 if it exists. Otherwise throw std::string error.
+*
+* @param: int site0
+*       : int site1
+*
+* @return: bool
+*/
+bool Graph::GetLink(int site0, int site1) {
+	if (IsInGraph(site0) && IsInGraph(site1)) {
+		if (AreNeighbours(site1, site0)) {
+			return mGraph[site0].neighbours[site1];
+		} else {
+			// Error handling for site1 not neighbour to site0
+			std::stringstream ss;
+			ss << "Call to Graph::GetLink failed since "
+				<< site1 << " is not a neighbour to " << site0 << "\n";
+			throw ss.str();
+		}
+	} else {
+		// Error handling for site0 not in mGraph
+		std::stringstream ss;
+		ss << "Call to Graph::GetLink failed since one of "
+			<< site0 << " and " << site1 << " is not in mGraph" << "\n";
+		throw ss.str();
+	}
+}
---
+++ cpp/source/xy/Site.cpp
@@ -0,0 +1,129 @@
+#include "Site.h"
+#include <cmath>
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+Site::Site(int index, int length, int dimension) {
+	// The index of this site
+	this->mRootIndex = index;
+	// Populate the neighbours unordered_map
+	AddNeighbours(length, dimension);
+}
+
+/**
+* @brief: Create the neighbours to site according to periodic boundary conditions
+*
+* @param: int length
+*       : int dimension
+*
+* @return: void
+*/
+void Site::AddNeighbours(int length, int dimension) {
+	// --- 2D ---
+	// x + y * L = N
+	//
+	// x = N - y * L
+	// y = N // L
+	//
+	// --- General ---
+	// x + y * L + z * L^2 + ... = N
+	bool startingWeight = 0;
+
+	// Find the (x, y, z, ...) vector corresponding to the given index (mRootIndex)
+	std::vector<int> xyz;
+	xyz.reserve(dimension);
+	auto temp_index = mRootIndex;
+	for (int i = 0; i < dimension; ++i) {
+
+		// std::cout << "Xyz index: " << i << "\n";
+		// std::cout << "Adding: temp_index % length : " << temp_index << " % " << length << " : " << temp_index % length << "\n";
+
+		xyz.push_back(temp_index % length);
+		temp_index = std::floor(temp_index / length);
+	}
+
+	// std::cout << "On site with index: " << mRootIndex << " with dimension: " << dimension << " and length: " << length << "\n";
+	// std::cout << "Found xyz: (";
+	// for (auto& x : xyz) {
+	// 	std::cout << x << ", ";
+	// }
+	// std::cout << ")" << "\n";
+
+	// Go through and check all directions, adding them to neighbours
+	for (auto& x_i : xyz) {
+		if ((x_i + 1) < length) {
+			// + direction is within the graph
+			x_i++;
+
+			// std::cout << "x_i on the + border. Adding: " << CalcIndexFromVector(xyz, length) << "\n";
+
+			neighbours.insert({CalcIndexFromVector(xyz, length), startingWeight});
+			// Make sure xyz is returned to its original state
+			x_i--;
+		} else {
+			// + direction was on border - Apply periodic boundary conditions
+			// x -> 0
+			auto x_old = x_i;
+			x_i = 0;
+
+			// std::cout << "x_i not on the + border. Adding: " << CalcIndexFromVector(xyz, length) << "\n";
+
+			neighbours.insert({CalcIndexFromVector(xyz, length), startingWeight});
+			// Make sure xyz is returned to its original state
+			x_i = x_old;
+		}
+
+		if (x_i != 0) {
+			// - direction is within the graph
+			x_i--;
+
+			// std::cout << "x_i on the - border. Adding: " << CalcIndexFromVector(xyz, length) << "\n";
+
+			neighbours.insert({CalcIndexFromVector(xyz, length), startingWeight});
+			// Make sure xyz is returned to its original state
+			x_i++;
+		} else {
+			// - direction was on border - Apply periodic boundary conditions
+			// x -> length - 1
+			auto x_old = x_i;
+			x_i = length - 1;
+
+			// std::cout << "x_i not on the - border. Adding: " << CalcIndexFromVector(xyz, length) << "\n";
+
+			neighbours.insert({CalcIndexFromVector(xyz, length), startingWeight});
+			// Make sure xyz is returned to its original state
+			x_i = x_old;
+		}
+	}
+}
+
+/**
+* @brief: Calculate the index corresponding to the values in xyz = (x_0, x_1, ..., x_N)
+*
+* @param: std::vector<int>& xyz
+*
+* @return: int
+*/
+int Site::CalcIndexFromVector(std::vector<int>& xyz, int length) {
+	// index = x_0 + L * x_1 + L^2 * x_2 + ...
+	auto index = 0;
+	auto exponent = 0;
+	for (auto& x_i : xyz) {
+		// x_i * L^i
+		index += x_i * std::pow(length, exponent);
+		exponent++;
+	}
+	return index;
+}
+
+/**
+* @brief: Return the index of the site
+*
+* @param: 
+*
+* @return: int
+*/
+int Site::GetIndex() {
+	return mRootIndex;
+}
