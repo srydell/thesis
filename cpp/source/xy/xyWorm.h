6a860a59b0410060e72e31310b0aa8a04d44e78d
srydell <simonwrydell@gmail.com>
2018-09-12 11:23:23 +0200

    GetSign is now a part of Graph. implemented IsAccepted for XY

--- cpp/source/xy/xyWorm.h
+++ cpp/source/xy/xyWorm.h
@@ -5,11 +5,9 @@
 #include <unordered_map>
 #include <vector>
 
-bool IsAccepted(double K, bool link_between, long double &random_num);
-void UpdateCorrelationFunction(int site0, int site1, int length, std::unordered_map<int, int> &correlation_func);
+bool IsAccepted(double K, int link_between, long double &random_num);
 void UpdateLoopLengths(std::unordered_map<int, int> &loop_lengths, std::unordered_map<int, std::vector<int>> &clusters, Graph &lattice);
-double GetAverageLoopLength(std::vector<int> &loop_lengths, double const &K);
-long double IsingSimulation(Graph & lattice, double K);
+long double XySimulation(Graph & lattice, double K);
 void WarmUp(int warm_up_runs, Graph& lattice, double K);
 
 #endif
