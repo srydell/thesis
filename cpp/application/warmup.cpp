964584d15f0038aa7f8095001476f31e8f611fef
srydell <simonwrydell@gmail.com>
2018-10-01 10:55:42 +0200

    Added closeToTc executable to run close to tc

--- cpp/application/warmup.cpp
+++ cpp/application/warmup.cpp
@@ -21,11 +21,8 @@ int main() {
 		int nulltime = time(nullptr);
 		srand((unsigned)nulltime + getpid());
 
-		// Create data files
-		auto windingnum_temp_file = GetUniqueFile("warmup_winding");
-
 		// How many different sizes of the simulation should run (L = 2^i)
-		for (auto& length : {8}) {
+		for (auto& length : {4, 8, 16, 32}) {
 
 			std::cout << "Dimension: " << dimension << "\n";
 			std::cout << "On length: " << length << "\n";
@@ -35,34 +32,13 @@ int main() {
 			Graph lattice(dimension, length, seed + getpid());
 
 			// Bond strength J = 1
-			double T = 2.2;
+			double T = 0.35;
 			double K = 1/T;
 
 			std::stringstream ss;
 			ss << "xyl" << length << "t" << T << ".txt";
 			std::string filename = ss.str();
-			if (FileExists(filename)) {
-				std::cout << "Loading file..." << "\n";
-				LoadGraphFromFile(filename, lattice);
-			} else {
-				std::cout << "File didn't exist" << "\n";
-			}
-			
-			windingnum_temp_file << "L=" << length << ":\n";
-
-			// Store the winding number
-			double winding_number_squared = 0;
-
-			int num_worms_started = 100'000;
-			for (int i = 0; i < num_worms_started; ++i) {
-				// Take measurement
-				WNandNS res = XySimulation(lattice, K);
-				winding_number_squared += std::pow(res.winding_number / 3, 2);
-
-				windingnum_temp_file << res.winding_number / 3 << "\n";
-
-			}
-			SaveGraphToFile(filename, lattice);
+			WarmUpAndSaveOrReload(10000 * length, lattice, K, filename);
 		}
 	} catch(std::string& error) {
 		std::cout << error << "\n";
