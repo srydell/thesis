682aa67973690b636b7e7ea6ddc215849242b805
srydell <simonwrydell@gmail.com>
2018-10-04 11:00:21 +0200

    More data

--- cpp/pyplot/winding_and_mats.py
+++ cpp/pyplot/winding_and_mats.py
@@ -24,7 +24,8 @@ if __name__ == '__main__':
     # The good data
     # simon_simulation_data = calc.process_file("./windingnum_tempXY3283", key=r"L=(\d+):", xy=r"(\d*\.?\d*) (\d*\.?\d*)")
     # simon_simulation_data = calc.process_file("./windingnum_tempXY17849", key=r"L=(\d+):", xy=r"(\d*\.?\d*) (\d*\.?\d*)")
-    simon_simulation_data = calc.process_file("./windingnum_tempXY20131", key=r"L=(\d+):", xy=r"(\d*\.?\d*) (\d*\.?\d*)")
+    # simon_simulation_data = calc.process_file("./windingnum_tempXY20131", key=r"L=(\d+):", xy=r"(\d*\.?\d*) (\d*\.?\d*)")
+    simon_simulation_data = calc.process_file("./windingnum_tempXY22525", key=r"L=(\d+):", xy=r"(\d*\.?\d*) (\d*\.?\d*)")
 
 
     colors = {4: "#b2b2b2", 6: "#999999", 8: "#7f7f7f", 10: "#666666", 12: "#4c4c4c",\
