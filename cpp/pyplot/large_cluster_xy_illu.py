726ec468c2b9017c0a6002f4aa5cc01e8b1b54d6
srydell <simonwrydell@gmail.com>
2018-11-07 16:34:23 +0100

    added presentation

--- cpp/pyplot/large_cluster_xy_illu.py
+++ cpp/pyplot/large_cluster_xy_illu.py
@@ -1,5 +1,5 @@
 '''
-Brief:               Plot a large 2d cluster
+Brief:               Plot a large 3d cluster
 
 File name:           large_cluster_illu.py
 Author:              Simon Rydell
@@ -33,27 +33,26 @@ if __name__ == '__main__':
     # Color of flux
     c = const.COLOR_MAP["red"]
 
+    ax.axis('off')
     # Load graph
     sites = ani.process_graph_file(f"xy/largest_cluster/largest_cluster", dimension, drawer)
     # Plot all flux
     for site in sites:
-        site.plot_arrows_to_neighbours(ax, system_size, color=c)
-
+        ax.scatter(*site.convert_to_xyz(site.i, system_size), c=const.COLOR_MAP["red"], s=3)
+        # site.plot_arrows_to_neighbours(ax, system_size, color=c)
 
     pdf_frames = []
+    theta = 30
     current_frame = -1
-    max_angle = 10
+    max_angle = 360
     for angle in np.arange(0, max_angle, 1):
         current_frame += 1
 
         print("\n=========================")
         print(f"On frame number: {current_frame}")
 
-        # Remove surrounding border
-        ax.axis('off')
-
         # Set viewing angle
-        ax.view_init(angle, 0)
+        ax.view_init(theta, angle)
 
         illu.save_figure(f"frames/xy/largest_cluster/{current_frame:03}")
         pdf_frames.append(f"plots/frames/xy/largest_cluster/{current_frame:03}.pdf")
@@ -64,5 +63,5 @@ if __name__ == '__main__':
     print(f"Merging the pdfs into {output_file}.pdf")
     ani.merge_pdfs(pdf_frames, output_file)
 
-    print(f"Creating the gif {output_file}.gif")
-    ani.pdf2gif(output_file, output_file)
+#     print(f"Creating the gif {output_file}.gif")
+#     ani.pdf2gif(output_file, output_file)
